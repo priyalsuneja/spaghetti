1ea376fa13f5d2a29d0bfecede61f55484012076
Priyal Suneja <psuneja@ucsd.edu>
2018-11-14 01:06:14 -0800

    made table? appear?

--- Assets/JSONParser.cs
+++ Assets/JSONParser.cs
@@ -13,7 +13,6 @@ public class JSONParser {
         public string id;
         public string[] variables;
 
-        // temp as JSON isn't working
         public Result()
         {
             LevelNumber = 5;
@@ -42,7 +41,7 @@ public class JSONParser {
         string json = 
             "{\"id\":2,\"jsonrpc\":\"2.0\",\"result\":{\"LevelNumber\":5,\"data\":[[[1,7,0],[2,7,1],[3,7,3],[4,7,6],[5,7,10],[6,7,15],[7,7,21]],[],[]],\"goal\":\"verify\",\"hint\":null,\"id\":\"s - gauss_sum_true - unreach - call - auto\",\"lvlSet\":\"fb\",\"startingInvs\":[],\"typeEnv\":{\"i\":\"int\",\"n\":\"int\",\"sum\":\"int\"},\"variables\":[\"i\",\"n\",\"sum\"]}}";
 
-        //Message m = JsonConvert.DeserializeObject<Message>(json);
+        Message m = JsonConvert.DeserializeObject<Message>(json);
 
     }
 	
--- Assets/TableCreator.cs
+++ Assets/TableCreator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,16 +8,19 @@ public class TableCreator : MonoBehaviour {
 
     int[,] data;
     string[] variables;
-    public int cols;
-    public int rows;
-    double startY = -628.4;
-    double startX = -1029;
-    double yDiff = 175;
-    double xDiff = 216;
-    double varY = -426;
+    int cols;
+    int rows;
+    public GameObject canvas;
+    public Font font;
+    float startX = -405;//-628;
+    float startY = 621;//-1029;
+    float canvasX = 720;
+    float canvasY = 1280;
+    float yDiff = 175;
+    float xDiff = 300;
+    float varY = -426;
 
-    //public Text textLol;
-    public Text[,] textArray;
+     public GameObject[,] textArray;
 
     // Use this for initialization
     void Start () {
@@ -24,12 +28,31 @@ public class TableCreator : MonoBehaviour {
         data = json.result.data;
         variables = json.result.variables;
         cols = variables.Length; // num of vars
+        Console.WriteLine("cols: " + cols);
         rows = data.Length; // num of data points
+        Console.WriteLine("rows: " + rows);
+        textArray = new GameObject[rows, cols];
 
-        textArray[0, 0] = gameObject.AddComponent<Text>();
-
-
-
+        
+        for( int i = 0; i < rows; i ++)
+        {
+            for( int j = 0; j < cols; j++)
+            {
+                GameObject newObj = new GameObject();
+                newObj.transform.SetParent(canvas.transform);
+                newObj.name = "(" + i + ", " + j + ")";
+                newObj.AddComponent<RectTransform>();
+                newObj.GetComponent<RectTransform>().sizeDelta = new Vector2(225, 225);
+                //newObj.GetComponent<RectTransform>().position = 
+                textArray[i, j] = newObj;
+                Text myText = newObj.AddComponent<Text>();
+                myText.text = i + ", " + j;
+                myText.fontSize = 91;
+                myText.font = font;
+                myText.alignment = TextAnchor.MiddleCenter;
+                textArray[i, j].transform.position = new Vector3(startX + 850 + (xDiff * j), startY + 1300 - (yDiff * i), 0);
+            }
+        }
     }
 	
 	// Update is called once per frame
