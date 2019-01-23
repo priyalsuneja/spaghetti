b69093242faf6c2642e5421c7c02d58774e68ae0
Caroline Sih <csih@ucsd.edu>
2019-01-23 12:04:49 -0800

    Main Menu Integration

---
+++ Assets/PlayTransition.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class PlayTransition : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
